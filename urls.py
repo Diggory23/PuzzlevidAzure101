58495a08dabf6406226feb16049e8643e4d064a9
Diggory23 <62451200+Diggory23@users.noreply.github.com>
2021-04-13 14:21:12 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.index, name = 'index'),
+    path('juega/',views.juega, name='juega'),
+    path('steam/',views.steam, name='steam'),
+    path('<int:pk>/', views.usuarios, name="usuarios")
+]
