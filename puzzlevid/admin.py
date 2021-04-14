23e24864121254bb815e2adca92d6d6662056ea2
Diggory23 <62451200+Diggory23@users.noreply.github.com>
2021-04-14 12:33:43 -0500

    Update admin.py

--- puzzlevid/admin.py
+++ puzzlevid/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-#from .models import Usuario
+from .models import Usuario, Session, Nivel, Intento
 
 # Register your models here.
-#admin.site.register(Usuario)
+admin.site.register(Usuario)
+admin.site.register(Session)
+admin.site.register(Nivel)
+admin.site.register(Intento)
