aedb8c912f012e833bf77f50da643c2f7f305f93
Diggory23 <A01652067@itesm.mx>
2021-04-15 10:37:30 -0500

    data

--- puzzlevid/views.py
+++ puzzlevid/views.py
@@ -77,5 +77,5 @@ def estadisticas(request):
         }
     print(retorno)
     '''
-    return render(request, 'estadisticas.html', {"data":rows})
+    return render(request, 'estadisticas.html', {"data":data})
     
