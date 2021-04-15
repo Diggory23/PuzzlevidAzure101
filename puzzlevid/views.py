61d60911b95aa96da32719a87a411b2291976705
Diggory23 <62451200+Diggory23@users.noreply.github.com>
2021-04-15 00:40:40 -0500

    Update views.py

--- puzzlevid/views.py
+++ puzzlevid/views.py
@@ -22,8 +22,8 @@ def estadisticas(request):
     #Create a connection credentials to the PostgreSQL database
     try:
         connection = psycopg2.connect(
-            user = "admin",
-            password = "ragnar",
+            user = "ragnar",
+            password = "Raramuri23%",
             host = "localhost",
             port = "5432",
             database = "puzzlevid"
