09ae49e7888f62984e5838abd92bdd703b21c8b6
Diggory23 <A01652067@itesm.mx>
2021-04-22 20:26:31 -0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import login, logout_then_login
+from django.contrib.auth import login
 
 
 urlpatterns = [
