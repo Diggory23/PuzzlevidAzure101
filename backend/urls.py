1295699148cda2d33d534eac8e891efc70f9f9b4
Diggory23 <A01652067@itesm.mx>
2021-04-22 19:43:27 -0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import login,logout_then_login
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('puzzlevid.urls')),
-    path('login/',login,{'template_name':'login.html'}, name = 'login')
+    path('login/', include('django.contrib.auth.urls'))
 ]
