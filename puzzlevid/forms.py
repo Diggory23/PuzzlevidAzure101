fa1ae69fb03b8cca276901a06efc1f744295c90e
Diggory23 <A01652067@itesm.mx>
2021-04-23 19:27:27 -0500

    Update forms.py

--- puzzlevid/forms.py
+++ puzzlevid/forms.py
@@ -1,14 +1,14 @@
 from django import forms 
-from django.contrib.auth.models import UserCreationForm
+#from django.contrib.auth.models import UserCreationForm
 from .models import Usuario
 
 
-class RegistrationForm(UserCreationForm):
+#class RegistrationForm(UserCreationForm):
     #email = forms.EmailField(max_length=60, help_text='Requerido. Por favor ingrese una direccion de e-mail correcta.')
 
-    class Meta:
-        model = Usuario
-        fields = ('nombre','apellido','nacimiento','gametag','email','password')
+    #class Meta:
+        #model = Usuario
+        #fields = ('nombre','apellido','nacimiento','gametag','email','password')
 
 
 
